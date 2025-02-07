b14fe61b843899cc37b422fad3312b74bffa10cd
Angel Ayala <angel4ayala@gmail.com>
2025-02-06 22:58:16 -0300

    adding SRL version of SAC

---
+++ learn_sac_cf.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 12:45:59 2025
+
+@author: angel
+"""
+import argparse
+
+from stable_baselines3 import SAC
+from stable_baselines3.sac.policies import SACPolicy
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+from sb3_srl.sac_srl import SRLSACPolicy, SRLSAC
+
+from utils import parse_crazyflie_env_args
+from utils import parse_memory_args
+from utils import parse_srl_args
+from utils import parse_training_args
+from utils import parse_utils_args
+from utils import instance_env
+from utils import wrap_env
+from utils import args2ae_config
+from utils import args2logpath
+from utils import ArgsSaveCallback
+
+
+def parse_agent_args(parser):
+    arg_agent = parser.add_argument_group('Agent')
+    arg_agent.add_argument("--lr", type=float, default=3e-4,
+                           help='Critic function Adam learning rate.')
+    arg_agent.add_argument("--tau", type=float, default=0.005,
+                           help='Soft target update \tau.')
+    arg_agent.add_argument("--discount-factor", type=float, default=0.99,
+                           help='Discount factor \gamma.')
+    arg_agent.add_argument("--train-freq", type=int, default=1,
+                           help='Steps interval for actor batch training.')
+    arg_agent.add_argument("--target-update-freq", type=int, default=1,
+                           help='Steps interval for target network update.')
+    return arg_agent
+
+
+def parse_args():
+    # Argument parser
+    parser = argparse.ArgumentParser()
+
+    parse_crazyflie_env_args(parser)
+    parse_agent_args(parser)
+    parse_memory_args(parser)
+    parse_srl_args(parser)
+    parse_training_args(parser)
+    parse_utils_args(parser)
+
+    return parser.parse_args()
+
+
+args = parse_args()
+# th.manual_seed(args.seed)
+# np.random.seed(args.seed)
+
+# Environment
+environment_name = 'webots_drone:webots_drone/CrazyflieEnvContinuous-v0'
+env, env_params = instance_env(args, environment_name, seed=args.seed)
+env, env_params = wrap_env(env, env_params)  # observation preprocesing
+
+if args.is_srl:
+    algo, policy = SRLSAC, SRLSACPolicy
+    # Autoencoder parameters
+    ae_config = list(args2ae_config(args, env_params).items())
+    if len(ae_config) == 0:
+        raise ValueError("No SRL model selected")
+    ae_type, ae_params = ae_config[0]
+
+    # Policy args
+    policy_args = {
+        'ae_type': ae_type,
+        'ae_params': ae_params,
+        }
+else:
+    algo, policy = SAC, SACPolicy
+    policy_args = None
+
+# Save a checkpoint every N steps
+log_path, exp_name, run_id = args2logpath(args, 'sac')
+agents_path = f"./{log_path}/{exp_name}_{run_id+1}/agents"
+
+checkpoint_callback = CheckpointCallback(
+  save_freq=args.eval_interval,
+  save_path=agents_path,
+  name_prefix="rl_model",
+  save_replay_buffer=False,
+  save_vecnormalize=False,
+)
+
+# Save experiment arguments
+args_callback = ArgsSaveCallback(
+    args, f"./{log_path}/{exp_name}_{run_id+1}/arguments.json")
+
+# Create RL model
+model = algo(policy, env,
+             learning_rate=args.lr,
+             buffer_size=args.memory_capacity,  # 1e6
+             learning_starts=args.memory_steps,
+             batch_size=args.batch_size,
+             tau=args.tau,
+             gamma=args.discount_factor,
+             train_freq=args.train_freq,
+             gradient_steps=1,
+             action_noise=None,
+             ent_coef="auto",
+             target_update_interval=args.target_update_freq,
+             target_entropy="auto",
+             use_sde=False,
+             sde_sample_freq=-1,
+             use_sde_at_warmup=False,
+             stats_window_size=100,
+             tensorboard_log=log_path,
+             policy_kwargs=policy_args,
+             verbose=0,
+             seed=args.seed,
+             device="auto",
+             _init_setup_model=True)
+
+# Train the agent
+model.learn(total_timesteps=args.steps,
+            callback=[checkpoint_callback, args_callback],
+            log_interval=4,
+            progress_bar=True,
+            tb_log_name=exp_name)
+model.save(f"./{agents_path}/rl_model_final")
---
+++ sb3_srl/sac_srl.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 22:27:14 2025
+
+@author: angel
+"""
+from typing import Optional
+import copy
+import numpy as np
+import torch as th
+from torch.nn import functional as F
+
+from stable_baselines3.common.policies import ContinuousCritic
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.type_aliases import PyTorchObs
+from stable_baselines3.common.utils import get_parameters_by_name, polyak_update
+
+from stable_baselines3 import SAC
+from stable_baselines3.sac.policies import Actor
+from stable_baselines3.sac.policies import SACPolicy
+
+from sb3_srl.autoencoders import instance_autoencoder
+
+
+class SRLSACPolicy(SACPolicy):
+    def __init__(self, *args,
+                 ae_type: str = 'Vector', ae_params : dict = {},
+                 **kwargs):
+        self.features_dim = ae_params['latent_dim']
+        super(SRLSACPolicy, self).__init__(*args, **kwargs)
+
+        self.make_autencoder(ae_type, ae_params)
+        self.encoder_target = copy.deepcopy(self.ae_model.encoder)
+        self.encoder_target.train(False)
+
+    def make_autencoder(self, ae_type, ae_params):
+        self.ae_model = instance_autoencoder(ae_type, ae_params)
+        self.ae_model.adam_optimizer(ae_params['encoder_lr'],
+                                     ae_params['decoder_lr'])
+
+    def make_actor(self, features_extractor: Optional[BaseFeaturesExtractor] = None) -> Actor:
+        actor_kwargs = self._update_features_extractor(self.actor_kwargs, features_extractor)
+        actor_kwargs["features_dim"] = self.features_dim
+        return Actor(**actor_kwargs).to(self.device)
+
+    def make_critic(self, features_extractor: Optional[BaseFeaturesExtractor] = None) -> ContinuousCritic:
+        critic_kwargs = self._update_features_extractor(self.critic_kwargs, features_extractor)
+        critic_kwargs["features_dim"] = self.features_dim
+        return ContinuousCritic(**critic_kwargs).to(self.device)
+
+    def _predict(self, observation: PyTorchObs, deterministic: bool = False) -> th.Tensor:
+        # Note: the deterministic deterministic parameter is ignored in the case of TD3.
+        #   Predictions are always deterministic.
+        with th.no_grad():
+            obs_z = self.ae_model.encoder(observation)
+        return self.actor(obs_z)
+
+    def set_training_mode(self, mode: bool) -> None:
+        self.actor.set_training_mode(mode)
+        self.critic.set_training_mode(mode)
+        self.ae_model.set_training_mode(mode)
+        self.training = mode
+
+class SRLSAC(SAC):
+    def __init__(self, *args, **kwargs):
+        super(SRLSAC, self).__init__(*args, **kwargs)
+
+    def _create_aliases(self) -> None:
+        super()._create_aliases()
+        self.encoder = self.policy.ae_model.encoder
+        self.decoder = self.policy.ae_model.decoder
+        self.encoder_target = self.policy.encoder_target
+
+    def _setup_model(self) -> None:
+        super()._setup_model()
+        self.policy.ae_model.to(self.device)
+        # Running mean and running var
+        self.encoder_batch_norm_stats = get_parameters_by_name(self.encoder, ["running_"])
+        self.encoder_batch_norm_stats_target = get_parameters_by_name(self.encoder_target, ["running_"])
+
+    def train(self, gradient_steps: int, batch_size: int = 64) -> None:
+        # Switch to train mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(True)
+        # Update optimizers learning rate
+        optimizers = [self.actor.optimizer, self.critic.optimizer]
+        if self.ent_coef_optimizer is not None:
+            optimizers += [self.ent_coef_optimizer]
+
+        # Update learning rate according to lr schedule
+        self._update_learning_rate(optimizers)
+
+        ent_coef_losses, ent_coefs = [], []
+        actor_losses, critic_losses = [], []
+        ae_losses, l2_losses = [], []
+
+        for gradient_step in range(gradient_steps):
+            # Sample replay buffer
+            replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)  # type: ignore[union-attr]
+
+            with th.no_grad():
+                obs_z = self.encoder(replay_data.observations)
+                next_obs_z = self.encoder_target(replay_data.next_observations)
+
+            # We need to sample because `log_std` may have changed between two gradient steps
+            if self.use_sde:
+                self.actor.reset_noise()
+
+            # Action by the current actor for the sampled state
+            actions_pi, log_prob = self.actor.action_log_prob(obs_z)
+            log_prob = log_prob.reshape(-1, 1)
+
+            ent_coef_loss = None
+            if self.ent_coef_optimizer is not None and self.log_ent_coef is not None:
+                # Important: detach the variable from the graph
+                # so we don't change it with other losses
+                # see https://github.com/rail-berkeley/softlearning/issues/60
+                ent_coef = th.exp(self.log_ent_coef.detach())
+                ent_coef_loss = -(self.log_ent_coef * (log_prob + self.target_entropy).detach()).mean()
+                ent_coef_losses.append(ent_coef_loss.item())
+            else:
+                ent_coef = self.ent_coef_tensor
+
+            ent_coefs.append(ent_coef.item())
+
+            # Optimize entropy coefficient, also called
+            # entropy temperature or alpha in the paper
+            if ent_coef_loss is not None and self.ent_coef_optimizer is not None:
+                self.ent_coef_optimizer.zero_grad()
+                ent_coef_loss.backward()
+                self.ent_coef_optimizer.step()
+
+            with th.no_grad():
+                # Select action according to policy
+                next_actions, next_log_prob = self.actor.action_log_prob(next_obs_z)
+                # Compute the next Q values: min over all critics targets
+                next_q_values = th.cat(self.critic_target(next_obs_z, next_actions), dim=1)
+                next_q_values, _ = th.min(next_q_values, dim=1, keepdim=True)
+                # add entropy term
+                next_q_values = next_q_values - ent_coef * next_log_prob.reshape(-1, 1)
+                # td error + entropy term
+                target_q_values = replay_data.rewards + (1 - replay_data.dones) * self.gamma * next_q_values
+
+            # Get current Q-values estimates for each critic network
+            # using action from the replay buffer
+            current_q_values = self.critic(obs_z, replay_data.actions)
+
+            # Compute critic loss
+            critic_loss = 0.5 * sum(F.mse_loss(current_q, target_q_values) for current_q in current_q_values)
+            assert isinstance(critic_loss, th.Tensor)  # for type checker
+            critic_losses.append(critic_loss.item())  # type: ignore[union-attr]
+
+            # Optimize the critic
+            self.critic.optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic.optimizer.step()
+            
+            # Compute reconstruction loss
+            rep_loss, [rec_loss, latent_loss] = self.policy.ae_model.compute_representation_loss(
+                replay_data.observations, replay_data.actions, replay_data.next_observations)
+            ae_losses.append(rec_loss.item())
+            l2_losses.append(latent_loss.item())
+            self.policy.ae_model.update_representation(rep_loss)
+
+            # Compute actor loss
+            # Alternative: actor_loss = th.mean(log_prob - qf1_pi)
+            # Min over all critic networks
+            q_values_pi = th.cat(self.critic(obs_z, actions_pi), dim=1)
+            min_qf_pi, _ = th.min(q_values_pi, dim=1, keepdim=True)
+            actor_loss = (ent_coef * log_prob - min_qf_pi).mean()
+            actor_losses.append(actor_loss.item())
+
+            # Optimize the actor
+            self.actor.optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor.optimizer.step()
+
+            # Update target networks
+            if gradient_step % self.target_update_interval == 0:
+                polyak_update(self.critic.parameters(), self.critic_target.parameters(), self.tau)
+                polyak_update(self.encoder.parameters(), self.encoder_target.parameters(), self.tau)
+                # Copy running stats, see GH issue #996
+                polyak_update(self.batch_norm_stats, self.batch_norm_stats_target, 1.0)
+                polyak_update(self.encoder_batch_norm_stats, self.encoder_batch_norm_stats_target, 1.0)
+
+        self._n_updates += gradient_steps
+
+        self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
+        self.logger.record("train/ent_coef", np.mean(ent_coefs))
+        self.logger.record("train/actor_loss", np.mean(actor_losses))
+        self.logger.record("train/critic_loss", np.mean(critic_losses))
+        self.logger.record("train/ae_loss", np.mean(ae_losses))
+        self.logger.record("train/l2_loss", np.mean(l2_losses))
+        if len(ent_coef_losses) > 0:
+            self.logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))
